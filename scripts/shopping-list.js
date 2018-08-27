e365118a64e6792c693d35d0d7212197737fce78
Nikolas Melgarejo <nikoclops@gmail.com>
2018-08-27 14:17:36 -0400

    Shopping list uses store methods instead of global functions

--- scripts/shopping-list.js
+++ scripts/shopping-list.js
@@ -1,3 +1,4 @@
+'use strict'
 /* global store, Item */
 
 // eslint-disable-next-line no-unused-vars
@@ -75,10 +76,7 @@ const shoppingList = (function(){
     });
   }
   
-  function toggleCheckedForListItem(id) {
-    const foundItem = store.items.find(item => item.id === id);
-    foundItem.checked = !foundItem.checked;
-  }
+
   
   
   function getItemIdFromElement(item) {
@@ -90,19 +88,9 @@ const shoppingList = (function(){
   function handleItemCheckClicked() {
     $('.js-shopping-list').on('click', '.js-item-toggle', event => {
       const id = getItemIdFromElement(event.currentTarget);
-      toggleCheckedForListItem(id);
+      store.findAndToggleChecked(id);
       render();
     });
-  }
-  
-  function deleteListItem(id) {
-    const index = store.items.findIndex(item => item.id === id);
-    store.items.splice(index, 1);
-  }
-  
-  function editListItemName(id, itemName) {
-    const item = store.items.find(item => item.id === id);
-    item.name = itemName;
   }
   
   function toggleCheckedItemsFilter() {
@@ -120,7 +108,7 @@ const shoppingList = (function(){
       // get the index of the item in store.items
       const id = getItemIdFromElement(event.currentTarget);
       // delete the item
-      deleteListItem(id);
+      store.findAndDelete(id);
       // render the updated shopping list
       render();
     });
@@ -131,7 +119,7 @@ const shoppingList = (function(){
       event.preventDefault();
       const id = getItemIdFromElement(event.currentTarget);
       const itemName = $(event.currentTarget).find('.shopping-item').val();
-      editListItemName(id, itemName);
+      store.findAndUpdateName(id, itemName);
       render();
     });
   }
