af81a5f0169471fc814ed42450b6596ed8c6513b
Nikolas Melgarejo <nikoclops@gmail.com>
2018-08-27 12:26:07 -0400

    added a findById, addItem, and findAndToggleChecked method onto the store object

--- scripts/store.js
+++ scripts/store.js
@@ -1,3 +1,5 @@
+'use strict';
+/* global Item */
 
 const store = (function(){
   const hideCheckedItems = false;
@@ -10,10 +12,33 @@ const store = (function(){
     { id: cuid(), name: 'bread', checked: false }
   ];
 
+  const findById = function(id) {
+    return this.items.find(obj => obj.id === id);
+  };
+
+  const addItem = function(name) {
+    try {
+      Item.validateName(name);
+      this.items.push(Item.create(name));
+    }catch(e) {
+      console.log(e.message);
+    }
+  };
+
+  const findAndToggleChecked = function(id) {
+    const currentItem = this.findById(id);
+    currentItem.checked = !currentItem.checked;
+  };
+  
+  
+
   return {
     items,
     hideCheckedItems,
-    searchTerm
+    searchTerm,
+    findById,
+    addItem,
+    findAndToggleChecked
   };
 
 }());
